21d973abbddfe5419a08366780553267fb36309a
hmsuna <70170393+hmsuna@users.noreply.github.com>
2021-09-01 23:56:58 -0500

    Moved file to correct folder

---
+++ main.cpp
@@ -0,0 +1,29 @@
+/*
+	Haley Montgomery
+	C++ Semester Year
+	Due:09/01/2021
+	Lab 1 
+	My First Project
+
+	Testing my first C++ code. Testing project to place in repository.
+
+*/
+
+//	
+
+#include <iostream>		
+
+/*	Begin the main function. Every C++ program must have a main function. This is where
+	every C++ program will start. The int is a function return type.
+*/
+int main()
+{
+	// Using the standard namespace, use the cout method to output the string to the
+	// standard output device (usually the screen, but not always).
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "My favorite color is purple!" << std::endl;
+
+	// Complete the function by returning the integer 0 to main. This tells is a way
+	// of saying that the main function completed with no errors.
+	return 0;
+}
